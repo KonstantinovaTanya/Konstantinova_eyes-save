935a3a76c5361764340f2fe73764ce0582bdc61e
Таня <@DESKTOP-8ARU1LD>
2026-02-12 14:13:38 +0500

    Добавьте файлы проекта.

---
+++ Konstantinova_eyes-save/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Konstantinova_eyes_save
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
