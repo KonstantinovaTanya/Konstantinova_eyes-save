5514d47127a46bc34d191d72e8c6dd1a78d231e4
Таня Константинова <konstantinovat14@gmail.com>
2026-02-18 09:24:18 +0500

    ЛБ17

--- Konstantinova_eyes-save/AgentPage.xaml.cs
+++ Konstantinova_eyes-save/AgentPage.xaml.cs
@@ -23,11 +23,88 @@ namespace Konstantinova_eyes_save
         public AgentPage()
         {
             InitializeComponent();
+            var currentAgents = Konstantinova_eyesEntities.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = currentAgents;
+
+            ComboType.SelectedIndex = 0;
+            SortComboBox.SelectedIndex = 0;
+
+            UpdateAgents();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void UpdateAgents()
+        {
+            var currentAgents = Konstantinova_eyesEntities.GetContext().Agent.ToList();
+
+            string searchText = TBoxSearch.Text?.ToLower() ?? "";
+
+            string cleanSearchText = new string(searchText.Where(c => char.IsDigit(c)).ToArray());
+
+            if (cleanSearchText.StartsWith("8"))
+            {
+                cleanSearchText = "7" + cleanSearchText.Substring(1);
+            }
+
+            currentAgents = currentAgents.Where(p =>
+                   
+                    (!string.IsNullOrEmpty(p.Title) && p.Title.ToLower().Contains(searchText)) ||
+                  
+                    (!string.IsNullOrEmpty(p.Email) && p.Email.ToLower().Contains(searchText)) ||
+                 
+                    (!string.IsNullOrEmpty(p.Phone) && !string.IsNullOrEmpty(cleanSearchText) &&
+                     new string(p.Phone.Where(c => char.IsDigit(c)).ToArray()).Contains(cleanSearchText))).ToList();
+
+
+            switch (SortComboBox.SelectedIndex)
+            {
+                case 1: 
+                    currentAgents = currentAgents.OrderBy(p => p.Title).ToList(); break;
+                case 2: 
+                    currentAgents = currentAgents.OrderByDescending(p => p.Title).ToList(); break;
+                case 3: 
+                    currentAgents = currentAgents.OrderBy(p => p.Discount).ToList(); break;
+                case 4: 
+                    currentAgents = currentAgents.OrderByDescending(p => p.Discount).ToList(); break;
+                case 5: 
+                    currentAgents = currentAgents.OrderBy(p => p.Priority).ToList(); break;
+                case 6: 
+                    currentAgents = currentAgents.OrderByDescending(p => p.Priority).ToList(); break;
+            }
+
+            
+            string selectedType = (ComboType.SelectedItem as TextBlock)?.Text;
+
+            if (ComboType.SelectedIndex > 0)
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == selectedType).ToList();
+
+
+            AgentListView.ItemsSource = currentAgents;
+        }
+
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void AgentListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
